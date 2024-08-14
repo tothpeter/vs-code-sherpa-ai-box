40763edd7de1b169c495be3f16de789c97295755
Peter Toth <tothpeter08@gmail.com>
2024-08-14 21:25:21 +0400

    First working sol

--- extension.js
+++ extension.js
@@ -1,3 +1,6 @@
+// const ollama = require('ollama');
+const ollama = require('ollama/browser');
+
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
@@ -17,14 +20,20 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('sherpa-ai-box.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Sherpa AI box!');
-	});
+	let disposable = vscode.commands.registerCommand('sherpa-ai-box.helloWorld', test);
 
 	context.subscriptions.push(disposable);
+}
+
+async function test() {
+	const response = await ollama.default.chat({
+		model: 'llama3.1',
+		messages: [{ role: 'user', content: 'How much is 1+1?' }],
+	})
+	console.log('----------------------')
+	console.log(response.message.content)
+
+	// vscode.window.showInformationMessage('Hello World from Sherpa AI box!');
 }
 
 // This method is called when your extension is deactivated
